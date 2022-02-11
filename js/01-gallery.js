c6cb72655571166a853a6a95c2dca777e3f6f1f8
fenota-viktoriya <Fen9206@gmail.com>
2022-02-11 19:06:47 +0200

    add task

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+//1 Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+const galleryRef = document.querySelector('.gallery');
+function createMarkup({ preview, original, description}){
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt=""${description}
+    />
+  </a>
+</div>`
+};
+const markup = galleryItems.map(createMarkup).join('');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+
+//2.Реализация делегирования на div.gallery и получение url большого изображения.
+galleryRef.addEventListener('click', onClickGallery);
+
+function onClickGallery(event){
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
+      createBasicLigtbox();
+};
+
+
+function createBasicLigtbox(){
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">     
+`,  {
+    onShow: () => {
+    
+      window.addEventListener("keydown", onClickEscape);
+    },
+    onClose: () => window.removeEventListener("keydown", onClickEscape),
+  }
+);
+function onClickEscape(event){
+    if (event.key === "Escape") {
+        instance.close();
+      }
+}
+instance.show()
+        
+};
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,18 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+//1 Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+const galleryRef = document.querySelector('.gallery');
+function createMarkup({ preview, original, description}){
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" title ="${description}" />
+  </a>`
+};
+const markup = galleryItems.map(createMarkup).join('');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery a');
+//console.dir(lightbox);
+lightbox.defaultOptions.captionDelay = 250;
