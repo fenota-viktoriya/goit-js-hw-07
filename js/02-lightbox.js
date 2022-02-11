ae6389aae3cc4d8c8f7e95a681ba623db4edca8c
fenota-viktoriya <Fen9206@gmail.com>
2022-02-11 19:14:54 +0200

    add new caption

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,7 @@ console.log(galleryItems);
 const galleryRef = document.querySelector('.gallery');
 function createMarkup({ preview, original, description}){
     return `<a class="gallery__item" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}" title ="${description}" />
+    <img class="gallery__image" src="${preview}" alt="${description}" />
   </a>`
 };
 const markup = galleryItems.map(createMarkup).join('');
@@ -16,3 +16,4 @@ galleryRef.insertAdjacentHTML('afterbegin', markup);
 let lightbox = new SimpleLightbox('.gallery a');
 //console.dir(lightbox);
 lightbox.defaultOptions.captionDelay = 250;
+lightbox.defaultOptions.captionsData = 'Alt';
